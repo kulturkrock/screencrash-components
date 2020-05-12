00bcdf5b12616f27b03e28556d5d73c98e76c36c
Matz Larsson <matzlarsson94@gmail.com>
2020-05-12 10:04:26 +0200

    Updated to try and match specification better
    * Encodes 0x00 at start of message, 0xFF at end of message
    * Decodes messages with 0x00 prefix and 0xFF suffix
    * Client sends announcement on connect (fix exact format later)
    * Client sends regular heartbeat pings with a every second

--- dummy_core/index.js
+++ dummy_core/index.js
@@ -6,10 +6,51 @@ wss.on('connection', function connection(ws) {
 
     console.log("Got connection!");
 
-    ws.on('message', function incoming(message) {
-        console.log('Received: %s', message);
+    ws.on('message', async function incoming(data) {
+        // Uncomment to test messages FROM client (may do it hard to enter commands)
+        let arrData = data.split(",").map(x => parseInt(x));
+        if (arrData[0] == 0x00 && arrData[arrData.length-1] == 0xFF){
+            let text = arrData.splice(1, arrData.length-2).map(x => String.fromCharCode(x)).join("");
+            let obj = JSON.parse(text);
+            console.log(JSON.stringify(obj));
+        } else {
+            console.log("Received invalid data: %s", data);
+        }
     });
 });
+
+/* Joinked from stack overflow. Appears on multiple places
+   so I dont really know who to give cred to */
+function toUTF8Array(str) {
+    let utf8 = [];
+    for (let i = 0; i < str.length; i++) {
+        let charcode = str.charCodeAt(i);
+        if (charcode < 0x80) utf8.push(charcode);
+        else if (charcode < 0x800) {
+            utf8.push(0xc0 | (charcode >> 6),
+                      0x80 | (charcode & 0x3f));
+        }
+        else if (charcode < 0xd800 || charcode >= 0xe000) {
+            utf8.push(0xe0 | (charcode >> 12),
+                      0x80 | ((charcode>>6) & 0x3f),
+                      0x80 | (charcode & 0x3f));
+        }
+        // surrogate pair
+        else {
+            i++;
+            // UTF-16 encodes 0x10000-0x10FFFF by
+            // subtracting 0x10000 and splitting the
+            // 20 bits of 0x0-0xFFFFF into two halves
+            charcode = 0x10000 + (((charcode & 0x3ff)<<10)
+                      | (str.charCodeAt(i) & 0x3ff));
+            utf8.push(0xf0 | (charcode >>18),
+                      0x80 | ((charcode>>12) & 0x3f),
+                      0x80 | ((charcode>>6) & 0x3f),
+                      0x80 | (charcode & 0x3f));
+        }
+    }
+    return utf8;
+}
 
 var stdin = process.openStdin();
 
@@ -20,11 +61,16 @@ stdin.addListener("data", function(d) {
         // Validate that it is JSON
         const cmd = JSON.parse(input);
 
+        // Construct output message
+        const output = toUTF8Array(input);
+        output.unshift(0x00);
+        output.push(0xFF);
+
         // Send to all
         let count = 0;
         wss.clients.forEach(function each(client) {
             if (client.readyState === WebSocket.OPEN) {
-                client.send(input);
+                client.send(output);
                 count++;
             }
         });
--- screen/model/coreConnection.js
+++ screen/model/coreConnection.js
@@ -5,27 +5,75 @@ module.exports = class CoreConnection {
         this.address = address;
         this.reconnect = reconnect;
         this.reconnectWait = reconnectWait;
+        this.socket = null;
+        this.heartbeatInterval = null;
         this.connect();
     }
 
+    getAnnounceMessage(){
+        return {
+            "type": "announce",
+            "component": "screen",
+            "channel": 1
+        };
+    }
+
+    getHeartbeatMessage(){
+        return {
+            "type": "heartbeat",
+            "component": "screen",
+            "channel": 1
+        };
+    }
+
     connect(){
+
+        if (this.heartbeatInterval != null){
+            clearInterval(this.heartbeatInterval);
+        }
+
         this.socket = new WebSocket(this.address);
         this.socket.addEventListener("open", this.onConnected.bind(this));
         this.socket.addEventListener("message", this.onMessage.bind(this));
         this.socket.addEventListener("close", this.onDisconnected.bind(this));
+
+        // TODO: Adjust time according to spec
+        this.heartbeatInterval = setInterval(this.sendHeartbeat.bind(this), 1000);
+    }
+
+    send(dataObj){
+        let output = this.dataToUTF8Array(JSON.stringify(dataObj));
+        output.unshift(0x00);
+        output.push(0xFF);
+        this.socket.send(output);
+    }
+
+    sendHeartbeat(){
+
+        if (this.socket != null && this.socket.readyState === WebSocket.OPEN){
+            this.send(this.getHeartbeatMessage());
+        }
     }
 
     onConnected(event){
+
         console.log("Connected to server: " + event.target.url);
+        this.send(this.getAnnounceMessage());
     }
 
-    onMessage(event){
-        const msg = JSON.parse(event.data);
-        console.log("Got message: ");
-        console.log(msg);
+    async onMessage(event){
+
+        let msg = await this.extractData(event.data);
+        if (msg != null){
+            console.log("Got message: ");
+            console.log(msg);
+        } else {
+            console.log("Warning: Got badly formatted message");
+        }
     }
 
     onDisconnected(event){
+
         if (!event.wasClean){
             if (this.reconnect){
                 console.log("Trying to reconnect in %d ms", this.reconnectWait);
@@ -36,5 +84,54 @@ module.exports = class CoreConnection {
         } else {
             console.log("Disconnected from server");
         }
+    }
+
+    async extractData(blob){
+
+        let data = await blob.arrayBuffer();
+        let view = new Int8Array(data);
+
+        // Make sure the formatting follows the spec
+        if (view[0] == 0x00 && view[view.length-1] == -1 /* 0xFF */) {
+            // Extract message
+            const strMessage = await new Response(blob.slice(1, blob.size-1)).text();
+            return JSON.parse(strMessage);
+        }
+
+        return null;
+    }
+
+    
+    /* Joinked from stack overflow. Appears on multiple places
+    so I dont really know who to give cred to */
+    dataToUTF8Array(str) {
+        let utf8 = [];
+        for (let i = 0; i < str.length; i++) {
+            let charcode = str.charCodeAt(i);
+            if (charcode < 0x80) utf8.push(charcode);
+            else if (charcode < 0x800) {
+                utf8.push(0xc0 | (charcode >> 6),
+                        0x80 | (charcode & 0x3f));
+            }
+            else if (charcode < 0xd800 || charcode >= 0xe000) {
+                utf8.push(0xe0 | (charcode >> 12),
+                        0x80 | ((charcode>>6) & 0x3f),
+                        0x80 | (charcode & 0x3f));
+            }
+            // surrogate pair
+            else {
+                i++;
+                // UTF-16 encodes 0x10000-0x10FFFF by
+                // subtracting 0x10000 and splitting the
+                // 20 bits of 0x0-0xFFFFF into two halves
+                charcode = 0x10000 + (((charcode & 0x3ff)<<10)
+                        | (str.charCodeAt(i) & 0x3ff));
+                utf8.push(0xf0 | (charcode >>18),
+                        0x80 | ((charcode>>12) & 0x3f),
+                        0x80 | ((charcode>>6) & 0x3f),
+                        0x80 | (charcode & 0x3f));
+            }
+        }
+        return utf8;
     }
 }

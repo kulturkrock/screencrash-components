dd2dca143a022b105e3af2801fcf92081ede1fa7
Matz Larsson <matzlarsson94@gmail.com>
2020-07-23 14:31:35 +0200

    Fixed const-ness

--- screen/renderer.js
+++ screen/renderer.js
@@ -1,8 +1,11 @@
 
 // Init command handler
-let CommandRouter = require('./model/commandrouter');
-let commandRouter = new CommandRouter(document);
+const CommandRouter = require('./model/commandrouter');
+const commandRouter = new CommandRouter(document);
 
 // Init connection to core
-let Connection = require('./model/coreconnection');
-let coreConnection = new Connection("ws://localhost:3000/", commandRouter.handleMessage.bind(commandRouter));
+const Connection = require('./model/coreconnection');
+const coreConnection = new Connection("ws://localhost:3000/", commandRouter.handleMessage.bind(commandRouter));
+
+// TODO: Do we need to save a reference to the core connection? It seems to handle itself rather nicely already.
+//       I left it there for future potential features with replacing core address etc.

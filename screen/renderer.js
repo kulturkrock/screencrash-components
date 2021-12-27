2cea25125ccb039e553f99ba99e3d85ae59a1948
Anton Neld <antonherrneld@gmail.com>
2021-12-27 13:35:43 +0100

    Connect to dummy_core by default

--- screen/renderer.js
+++ screen/renderer.js
@@ -6,7 +6,7 @@ const commandRouter = new CommandRouter(document);
 // Init connection to core
 const Connection = require('./model/coreconnection');
 // eslint-disable-next-line no-unused-vars
-const coreConnection = new Connection(`ws://${process.env.CORE}/`, commandRouter.handleMessage.bind(commandRouter));
+const coreConnection = new Connection(`ws://${process.env.CORE || 'localhost:3000'}/`, commandRouter.handleMessage.bind(commandRouter));
 
 // TODO: Do we need to save a reference to the core connection? It seems to handle itself rather nicely already.
 //       I left it there for future potential features with replacing core address etc.

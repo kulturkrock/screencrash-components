a69213afe11bd43128cecf981a7b05b23ecc5204
Matz Larsson <matzlarsson94@gmail.com>
2022-01-01 13:05:12 +0100

    Add support for events on effects (added, changed, removed) (#67)
    * Add support for events on effects (added, changed, removed)
    
    * Fix PR comments

--- audio/src/audio.py
+++ audio/src/audio.py
@@ -1,73 +1,167 @@
-import random
-import string
+import threading
 
 import os
+import time
+
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame as pg
 
 class AudioMixer:
 
-    def __init__(self, nof_max_sounds=20):
+    def __init__(self, event_callback, nof_max_sounds=20):
         pg.mixer.init()
         pg.init()
         pg.mixer.set_num_channels(nof_max_sounds)
+        self._event_callback = event_callback
         self._sounds = {}
+        self._sound_infos = {}
+        self._end_events = {}
+        self._current_end_event = 1000
 
-    def _generate_id(self):
+        self._event_handle_thread = threading.Thread(target=self._handle_pygame_event)
+        self._event_handle_thread.start()
+
+    def _emit(self, event_type, sound_id):
+        if self._event_callback:
+            self._event_callback(event_type, sound_id)
+        else:
+            print(f"Mixer wants to emit event {event_type} for {sound_id}")
+
+    def _handle_pygame_event(self):
         while True:
-            val = ''.join(random.choice(string.ascii_uppercase) for _ in range(16))
-            if not val in self._sounds:
-                return val
+            event = pg.event.wait()
+            if event.type in self._end_events:
+                sound_id = self._end_events[event.type]
+                self._emit("removed", sound_id)
 
-    def add(self, path, loops=0, autostart=False):
+    def add(self, sound_id, path, loops=0, autostart=False):
         sound = pg.mixer.Sound(path)
         free_channel = pg.mixer.find_channel()
         if free_channel is None:
             print("No free channels to play sound on. Oh noes!")
-            return None
+            return False
+
+        self._sounds[sound_id] = free_channel
+        self._sound_infos[sound_id] = {
+            "duration": sound.get_length(),
+            "current_time": 0,
+            "last_sync": int(time.time() * 1000),
+            "playing": autostart,
+            "muted": False,
+            "last_volume": -1,
+        }
+        free_channel.set_endevent(self._current_end_event)
+        self._end_events[self._current_end_event] = sound_id
+        self._current_end_event += 1
 
         free_channel.play(sound, loops=loops)
         if not autostart:
             free_channel.pause()
 
-        sound_id = self._generate_id()
-        self._sounds[sound_id] = free_channel
-        return sound_id
+        self._emit("added", sound_id)
+
+        return True
+
+    def _update_sound_info(self, sound_id, is_playing):
+        sound_info = self._sound_infos[sound_id]
+        now = int(time.time() * 1000)
+        if sound_info["playing"]:
+            sound_info["current_time"] += (now - sound_info["last_sync"])
+        sound_info["playing"] = is_playing
+        sound_info["last_sync"] = now
 
     def play(self, sound_id):
         sound_channel = self._sounds.get(sound_id)
         if sound_channel:
+            self._update_sound_info(sound_id, True)
             sound_channel.unpause()
+            self._emit("changed", sound_id)
         return sound_channel is not None
 
     def pause(self, sound_id):
         sound_channel = self._sounds.get(sound_id)
         if sound_channel:
+            self._update_sound_info(sound_id, False)
             sound_channel.pause()
+            self._emit("changed", sound_id)
         return sound_channel is not None
 
     def stop(self, sound_id):
         sound_channel = self._sounds.get(sound_id)
         if sound_channel:
             sound_channel.stop()
+            del self._sounds[sound_id]
+            del self._sound_infos[sound_id]
+            # No need to send event, will be caught automatically
         return sound_channel is not None
 
     def is_running(self, sound_id):
         sound_channel = self._sounds.get(sound_id)
-        return sound_channel or sound_channel.get_busy()
+        return sound_channel and sound_channel.get_busy()
 
     def get_volume(self, sound_id):
         sound_channel = self._sounds.get(sound_id)
-        return sound_channel.get_volume() if sound_channel else -1
+        if sound_channel:
+            sound = sound_channel.get_sound()
+            return round(sound.get_volume()*100) if sound else 0
+        return 0
 
-    def set_volume(self, sound_id, volume):
+    def get_duration(self, sound_id):
         sound_channel = self._sounds.get(sound_id)
         if sound_channel:
-            sound_channel.set_volume(volume)
-        return sound_channel is not None
+            sound = sound_channel.get_sound()
+            return sound.get_length() if sound else 0
+        return 0
 
-    def set_volume_stereo(self, sound_id, volume_left, volume_right):
+    def get_sound_info(self, sound_id):
+        return self._sound_infos.get(sound_id)
+
+    def set_volume(self, sound_id, volume, skip_event=False):
         sound_channel = self._sounds.get(sound_id)
-        if sound_channel:
-            sound_channel.set_volume(volume_left, volume_right)
-        return sound_channel is not None
+        sound_info = self._sound_infos.get(sound_id)
+        sound = None
+        if sound_channel and sound_info:
+            sound = sound_channel.get_sound()
+            if sound:
+                sound.set_volume(volume/100)
+                sound_info["last_volume"] = volume
+                if volume != 0:
+                    sound_info["muted"] = False
+                if not skip_event:
+                    self._emit("changed", sound_id)
+        return sound is not None
+
+    def set_volume_stereo(self, sound_id, volume_left, volume_right, skip_event=False):
+        sound_channel = self._sounds.get(sound_id)
+        sound_info = self._sound_infos.get(sound_id)
+        sound = None
+        if sound_channel and sound_info:
+            sound = sound_channel.get_sound()
+            if sound:
+                sound.set_volume(volume_left/100, volume_right/100)
+                sound_info["last_volume"] = (volume_left, volume_right)
+                if volume_left != 0 or volume_right != 0:
+                    sound_info["muted"] = False
+                if not skip_event:
+                    self._emit("changed", sound_id)
+        return sound is not None
+
+    def toggle_mute(self, sound_id):
+        sound_channel = self._sounds.get(sound_id)
+        sound_info = self._sound_infos.get(sound_id)
+        if sound_channel and sound_info:
+            if sound_info["muted"]:
+                if type(sound_info["last_volume"]) == tuple:
+                    left_vol, right_vol = sound_info["last_volume"]
+                    self.set_volume_stereo(sound_id, left_vol, right_vol, skip_event=True)
+                else:
+                    self.set_volume(sound_id, sound_info["last_volume"], skip_event=True)
+                sound_info["muted"] = False
+            else:
+                prev_volume = self.get_volume(sound_id)
+                self.set_volume(sound_id, 0)
+                sound_info["last_volume"] = prev_volume
+                sound_info["muted"] = True
+                
+            self._emit("changed", sound_id)
+
--- audio/src/commandhandler.py
+++ audio/src/commandhandler.py
@@ -1,12 +1,42 @@
-import json
-
+import time
+from pathlib import Path
 from audio import AudioMixer
 
 class CommandHandler:
 
     def __init__(self):
-        self._mixer = AudioMixer()
+        self._mixer = AudioMixer(self._handle_mixer_event)
+        self._custom_event_handler = None
         self._sounds = {}
+
+    def set_event_handler(self, event_handler):
+        self._custom_event_handler = event_handler
+
+    def _emit(self, data):
+        if self._custom_event_handler:
+            self._custom_event_handler(data)
+        else:
+            print(f"Got an event: {data}")
+
+    def _handle_mixer_event(self, event_type, entity_id):
+        if event_type == "removed":
+            self._emit({"messageType": "effect-removed", "entityId": entity_id})
+        elif event_type == "changed" or event_type == "added":
+            sound_info = self._mixer.get_sound_info(entity_id)
+            data = {
+                "messageType": f"effect-{event_type}",
+                "entityId": entity_id,
+                "name": Path(self._sounds.get(entity_id, "")).stem,
+                "duration": sound_info["duration"],
+                "currentTime": sound_info["current_time"],
+                "lastSync": sound_info["last_sync"],
+                "playing": sound_info["playing"],
+                "muted": sound_info["muted"],
+                "volume": self._mixer.get_volume(entity_id),
+            }
+            self._emit(data)
+        else:
+            print(f"Got unhandled event {event_type} with sound_id={entity_id}")
 
     def _create_error_msg(self, msg):
         result = {"messageType": "cmd-error", "msg": msg}
@@ -15,7 +45,7 @@ class CommandHandler:
     def handle_message(self, message):
         try:
             cmd = message["command"]
-            entity_id = message["entity_id"]
+            entity_id = message["entityId"]
             return self._handle_command(cmd, entity_id, message)
         except Exception as e:
             return self._create_error_msg(f"Failed to carry out command. {e}")
@@ -31,6 +61,8 @@ class CommandHandler:
             self._stop(entity_id)
         elif cmd == "set_volume":
             self._set_volume(entity_id, message)
+        elif cmd == "toggle_mute":
+            self._toggle_mute(entity_id)
         else:
             print("Unhandled message: {}".format(message))
             return self._create_error_msg("Unsupported command")
@@ -39,36 +71,38 @@ class CommandHandler:
         path = params["asset"]
         loops = params.get("loops", 1) - 1  # pygame uses zero indexing for this
         autostart = params.get("autostart", False)
-        sound_id = self._mixer.add(path=path, loops=loops, autostart=autostart)
-        if sound_id is None:
+        self._sounds[entity_id] = path
+
+        if not self._mixer.add(entity_id, path=path, loops=loops, autostart=autostart):
+            del self._sounds[entity_id]
             return self._create_error_msg("Unable to add sound. No more channels?")
-        self._sounds[entity_id] = sound_id
 
     def _play(self, entity_id):
-        sound_id = self._sounds.get(entity_id)
-        if not sound_id:
+        if entity_id not in self._sounds:
             return self._create_error_msg("Audio not found. Did you add it?")
-        self._mixer.play(sound_id)
+        self._mixer.play(entity_id)
 
     def _pause(self, entity_id):
-        sound_id = self._sounds.get(entity_id)
-        if not sound_id:
+        if entity_id not in self._sounds:
             return self._create_error_msg("Audio not found. Did you add it?")
-        self._mixer.pause(sound_id)
+        self._mixer.pause(entity_id)
 
     def _stop(self, entity_id):
-        sound_id = self._sounds.get(entity_id)
-        if not sound_id:
+        if entity_id not in self._sounds:
             return self._create_error_msg("Audio not found. Did you add it?")
-        self._mixer.stop(sound_id)
+        self._mixer.stop(entity_id)
+
+    def _toggle_mute(self, entity_id):
+        if entity_id not in self._sounds:
+            return self._create_error_msg("Audio not found. Did you add it?")
+        self._mixer.toggle_mute(entity_id)
 
     def _set_volume(self, entity_id, params):
-        sound_id = self._sounds.get(entity_id)
-        if not sound_id:
+        if entity_id not in self._sounds:
             return self._create_error_msg("Audio not found. Did you add it?")
-        if "volume_left" in params or "volume_right" in params:
-            left = params.get("volume_left", 0)
-            right = params.get("volume_right", 0)
-            self._mixer.set_volume_stereo(sound_id, left / 100, right / 100)
+        if "volumeLeft" in params or "volumeRight" in params:
+            left = params.get("volumeLeft", 0)
+            right = params.get("volumeRight", 0)
+            self._mixer.set_volume_stereo(entity_id, left, right)
         else:
-            self._mixer.set_volume(sound_id, params.get("volume", 50) / 100)
+            self._mixer.set_volume(entity_id, params.get("volume", 50))

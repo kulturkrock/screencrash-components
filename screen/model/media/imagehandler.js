7b6431b725de06807a80fb1232357b70680df0d2
Matz Larsson <matzlarsson94@gmail.com>
2022-01-05 01:38:29 +0100

    Add name property to image handler

--- screen/model/media/imagehandler.js
+++ screen/model/media/imagehandler.js
@@ -1,17 +1,20 @@
 
 const MediaHandler = require('./mediahandler');
+const path = require('path');
 
 module.exports = class ImageHandler extends MediaHandler {
 
     init(createMessage) {
         super.init(createMessage);
         this.uiWrapper.innerHTML = `<img id = 'image-${this.id}' class = 'image-media' src = '${createMessage.asset}'>`;
+        this.clipName = path.parse(createMessage.asset).name;
     }
 
     getState() {
         return {
             ...super.getState(),
-            effectType: 'image'
+            effectType: 'image',
+            name: this.clipName
         };
     }
 

f67c4fb8795eed51e53bc45a8b0e25f188a8e1aa
Anton Neld <antonherrneld@gmail.com>
2023-01-17 20:02:06 +0100

    Update raspberry script and readme

--- raspberry/README.md
+++ raspberry/README.md
@@ -1,4 +1,16 @@
 # Raspberry component
 
-This component talks to a Raspberry Pi through Bluetooth. It may morph into
-a more specific component later.
+This component talks to a Raspberry Pi through Bluetooth, and controls
+GPIO pins. It will currently be used for costumes in Ragnarök, but who
+knows how generic it will end up?
+
+## How to use
+
+You need a Raspberry Pi and a laptop, both with bluetooth.
+
+- Connect something between the ground pin and GPIO pin 14 on the Raspberry
+- Copy `raspberry_script.py` to the Raspberry Pi
+- Change `serverMACAddress` in `raspberry_script.py` to the MAC address of the laptop's bluetooth device
+- On the laptop, run `CORE=<core IP> make dev`
+- On the Raspberry, run `python raspberry_script.py`
+- You can now control whatever you connected from Core
--- raspberry/raspberry_script.py
+++ raspberry/raspberry_script.py
@@ -1,14 +1,25 @@
-# This script should be placed on the Raspberry that the component should talk to
-# Currently it only prints the messages to stdout
+# This should be copied to the Raspberry
+# Currently contains a hard-coded address to Anton's laptop,
+# and just a random pin
 import socket
+import RPi.GPIO as GPIO
 
 serverMACAddress = "0C:60:76:8B:10:F4"
 port = 5
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(14, GPIO.OUT)
 
 s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
 s.connect((serverMACAddress, port))
 print("connected")
 while 1:
     data = s.recv(1024)
-    print(data)
+    print(f"Got command: {data}")
+    if data == b"on":
+        print("Turning on pin 14")
+        GPIO.output(14, 1)
+    elif data == b"off":
+        print("Turning off pin 14")
+        GPIO.output(14, 0)
 s.close()

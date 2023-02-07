da3a7db3c60518d575a36cac1e1c4c6dbc6f4409
Anton Neld <antonherrneld@gmail.com>
2023-02-07 21:09:53 +0100

    Autostart script, make more robust

--- raspberry/README.md
+++ raspberry/README.md
@@ -9,8 +9,15 @@ knows how generic it will end up?
 You need a Raspberry Pi and a laptop, both with bluetooth.
 
 - Connect something between the ground pin and GPIO pin 14 on the Raspberry
-- Copy `raspberry_script.py` to the Raspberry Pi
-- Change `serverMACAddress` in `raspberry_script.py` to the MAC address of the laptop's bluetooth device
+- Copy `on_raspberry/screencrash_listener.py` to `/home/pi/` on the Raspberry
+- Copy `on_raspberry/screencrash_listener.service` to `/etc/systemd/system/` on the Raspberry
+- On the Raspberry, run
+
+  ```sh
+  sudo systemctl daemon-reload
+  sudo systemctl enable screencrash_listener
+  ```
+
+- Change `serverMACAddress` in `screencrash_listener.py` to the MAC address of the laptop's bluetooth device
 - On the laptop, run `CORE=<core IP> make dev`
-- On the Raspberry, run `python raspberry_script.py`
 - You can now control whatever you connected from Core
---
+++ raspberry/on_raspberry/screencrash_listener.py
@@ -0,0 +1,37 @@
+# This should be copied to the Raspberry
+# Currently contains a hard-coded address to Anton's laptop,
+# and just a random pin
+import socket
+import RPi.GPIO as GPIO
+import time
+
+serverMACAddress = "0C:60:76:8B:10:F4"
+port = 5
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(14, GPIO.OUT)
+
+
+while True:
+    print("Trying to connect...")
+    try:
+        s = socket.socket(
+            socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM
+        )
+        s.connect((serverMACAddress, port))
+        print("connected")
+        while 1:
+            data = s.recv(1024)
+            print(f"Got command: {data}")
+            if data == b"on":
+                print("Turning on pin 14")
+                GPIO.output(14, 1)
+            elif data == b"off":
+                print("Turning off pin 14")
+                GPIO.output(14, 0)
+    except OSError as e:
+        print(e)
+        print("Waiting 5s")
+        time.sleep(5)
+    finally:
+        s.close()

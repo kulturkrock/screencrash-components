30fc793c4716eb2d45ec501598fd8ad0456dc057
Anton Neld <antonherrneld@gmail.com>
2023-03-08 21:09:02 +0100

    Initial raspberry component (#115)
    Add initial LED controlled raspberry component
    
    This will be used in costumes to control LEDs from the tech booth.

---
+++ ledController/src/bluetoothconnection.py
@@ -0,0 +1,27 @@
+import socket
+
+class BluetoothConnection:
+
+    def __init__(self):
+        self.hostMACAddress = '0C:60:76:8B:10:F4' # MAC address of a Bluetooth adapter on this machine
+        self.port = 5 # Arbitrary, must match what raspberry uses
+        self.backlog = 1 # Not sure what this is
+        self.socket = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+        self.client = None
+        
+    def listen(self):
+        self.socket.bind((self.hostMACAddress,self.port))
+        self.socket.listen(self.backlog)
+        self.client, _ = self.socket.accept()
+        print("connected")
+        self.send_text("hello")
+    
+    def send_text(self, text):
+        print(f"Trying to send: {text}")
+        self.client.send(bytes(text, "UTF-8"))
+        print(f"Sent: {text}")
+
+    def close(self):
+        print("Closing socket")	
+        self.client.close()
+        self.socket.close()

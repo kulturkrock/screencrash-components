d916927fba4340307743031270d04543c1441e09
Matz Larsson <matzlarsson94@gmail.com>
2023-03-18 21:28:53 +0100

    Fix opacity bug (#126)

--- media/model/media/visualhandler.js
+++ media/model/media/visualhandler.js
@@ -49,7 +49,7 @@ module.exports = class VisualHandler extends MediaHandler {
             this.setVisible(true);
         }
 
-        if (msg.opacity) {
+        if (msg.opacity != null) {
             this.setOpacity(msg.opacity);
         }
 
